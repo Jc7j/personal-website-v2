cd18647d004be2b3170256f258f021a2625d439c
Jc7j <chiangjason19@gmail.com>
2020-03-27 14:47:37 -0700

    Added initial css to App.tsx and created Welcome component

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcomeContainer">
+      <p className="welcomeText">Hi, Welcome!</p>
+      <header className="fullNameText">Jason Chiang</header>
+      <p className="subheadingText">
+        Self-taught software engineer through research & development{' '}
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
